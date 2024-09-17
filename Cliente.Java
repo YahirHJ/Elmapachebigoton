4bce25a1443b5cc33c560b357eb21ac6502bf228
YahirHJ <hernanadezy1@gmail.com>
2024-09-16 23:16:26 -0600

    Create Cliente.Java

---
+++ Cliente.Java
@@ -0,0 +1,48 @@
+package com.barberia.Mapache_Bigoton.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String telefono;
+
+    // Constructor sin argumentos
+    public Cliente() {}
+
+    // Constructor con argumentos
+    public Cliente(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
